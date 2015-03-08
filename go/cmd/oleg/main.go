cce53283fb5357d643e374fefdafc9dcafca6ed1
Christine Dodrill <xena@yolo-swag.com>
2015-03-07 21:56:17 -0800

    Add Go client

---
+++ go/cmd/oleg/main.go
@@ -0,0 +1,57 @@
+/*
+Command oleg is a simple commandline interface to OlegDB.
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Xe/oleg/go"
+)
+
+var (
+	DB           *oleg.Database
+	HostFlag     = flag.String("host", "127.0.0.1", "hostname olegdb is running on")
+	PortFlag     = flag.String("port", "38080", "port olegdb is listening on")
+	DeleteAction = flag.Bool("delete", false, "delete this key from olegdb?")
+)
+
+func main() {
+	flag.Parse()
+
+	DB = oleg.Purchase(*HostFlag, *PortFlag)
+
+	var (
+		table string
+		key   string
+		value string
+
+		err error
+	)
+
+	table = flag.Arg(0)
+	key = flag.Arg(1)
+
+	if flag.NArg() == 3 {
+		value = flag.Arg(2)
+	}
+
+	if value != "" {
+		err = DB.Jar(table, key, value)
+	} else {
+		if *DeleteAction {
+			err = DB.Scoop(table, key)
+		} else {
+			var val string
+			val, err = DB.Unjar(table, key)
+			fmt.Println(val)
+		}
+	}
+
+	if err != nil {
+		fmt.Errorf("error: %s", err.Error())
+		os.Exit(1)
+	}
+}
